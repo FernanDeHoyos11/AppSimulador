ac05e8e93eb24193d9aa78e39542585652ca5686
FernanDeHoyos11 <fernandehoyos93@gmail.com>
2023-09-20 11:06:07 -0500

    add desing

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import {  SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, View } from "react-native";
 import { useState } from "react";
 import { Ciclica } from "./src/helpers/Ciclica";
 import { InputDx } from "./components/InputDx";
 import { InputGx } from "./components/InputGx";
-import { AppBar, Button, IconButton, TextInput } from "@react-native-material/core";
+import { AppBar, Badge, Button, IconButton, Snackbar, TextInput } from "@react-native-material/core";
 import { PolinomioBinario } from "./src/helpers/PolinomioBinario";
 import { Octicons } from '@expo/vector-icons';
+
 
 export default function App() {
   const [resultado, setResultado] = useState('');
@@ -13,132 +14,178 @@ export default function App() {
   const [Divisor, setDivisor] = useState('')
   const [inputCharge, setInputCharge] = useState(false);
   const [inputChargeV, setInputChargeV] = useState(false);
-  const [CRC, setCrc] = useState(""); 
+  const [CRC, setCrc] = useState("");
   const [resultVerificar, setResultVerificar] = useState('');
-  const [resultRecorrerVer, setResultRecorrerVer] = useState(""); 
-   
-  const handleDividendoChange = (text) => {
-    setDividendo(text);
-  }
+  const [resultRecorrerVer, setResultRecorrerVer] = useState("");
+  const [isNull, setIsNull] = useState("");
+  const [isVerificar, setIsVerificar] = useState(false);
+  const [isResult, setIsResult] = useState(false);
 
-  const handleDivisorChange = (text) => {
-    setDivisor(text);
-  }
-
-  // Función para mostrar el texto al hacer clic en el botón
-  const mostrarTextoClick = () => {
-    setInputCharge(true)
-  }
-  
 
   const Dx = PolinomioBinario.convertirAPolinomioBinario(Dividendo)
   const Gx = PolinomioBinario.convertirAPolinomioBinario(Divisor)
 
-    const calcularCRC = () => {
-
-      let digitosDespuesDelPrimerUno = 0;
-        let primerUnoEncontrado = false;
-        var longitud = Gx.length;
-
-        for (let i = 0; i < longitud; i++) {
-            const digito = Gx.charAt(i);
-            if (digito === '1') {
-                if (!primerUnoEncontrado) {
-                    primerUnoEncontrado = true;
-                } else {
-                    digitosDespuesDelPrimerUno++;
-                }
-            } else if (primerUnoEncontrado) {
-                digitosDespuesDelPrimerUno++;
-            }
-        }
-        var longitud = Gx.length;
-        const agregado = '0'.repeat(digitosDespuesDelPrimerUno);
-        const crc = agregado
-        const ciclica = new Ciclica(Dx, Gx, crc);
-        const resultadoCalculo = ciclica.imprime() + ciclica.recorrer(0, longitud);
-        setResultado(resultadoCalculo);
-        setInputChargeV(true)
-
-        const residuo = ciclica.getResiduo();
-        setCrc(residuo)
-        
-    }
-
-    const onResultSucces = () => {
-      var longitud = Gx.length;
-      const ciclica = new Ciclica(Dx, Gx, CRC);
-      const resultadoImprime = ciclica.imprime();
-      setResultVerificar(resultadoImprime);
-      const resultadoRecorrer = ciclica.recorrer(0, longitud);
-      setResultRecorrerVer(resultadoRecorrer);
+  const handleDividendoChange = (text) => {
+      const validInput = /^[xX^+\d\s]+$/;
+      if (validInput.test(text)) {
+        setDividendo(text);
+      }
   }
 
-    return (
+  const handleDivisorChange = (text) => {
+    const validInput = /^[xX^+\d\s]+$/;
+    if (validInput.test(text)) {
+      setDivisor(text);
+    }
+  }
 
-      <>
-     
+  // Función para mostrar el texto al hacer clic en el botón
+  const mostrarTextoClick = () => {
+    if (Dividendo.length > 2 && Divisor.length > 2) {
+      setInputCharge(true)
+    } else {
+      false
+      setInputCharge(false);
+      setIsNull('Agregue los polinomios a convertir')
+    }
+  }
+
+
+
+
+  const calcularCRC = () => {
+
+    let digitosDespuesDelPrimerUno = 0;
+    let primerUnoEncontrado = false;
+    var longitud = Gx.length;
+
+    for (let i = 0; i < longitud; i++) {
+      const digito = Gx.charAt(i);
+      if (digito === '1') {
+        if (!primerUnoEncontrado) {
+          primerUnoEncontrado = true;
+        } else {
+          digitosDespuesDelPrimerUno++;
+        }
+      } else if (primerUnoEncontrado) {
+        digitosDespuesDelPrimerUno++;
+      }
+    }
+    var longitud = Gx.length;
+    const agregado = '0'.repeat(digitosDespuesDelPrimerUno);
+    const crc = agregado
+    const ciclica = new Ciclica(Dx, Gx, crc);
+    const resultadoCalculo = ciclica.imprime() + ciclica.recorrer(0, longitud);
+    setResultado(resultadoCalculo);
+    setInputChargeV(true)
+
+    const residuo = ciclica.getResiduo();
+    setCrc(residuo)
+    setIsVerificar(true)
+
+  }
+
+  const onResultSucces = () => {
+    var longitud = Gx.length;
+    const ciclica = new Ciclica(Dx, Gx, CRC);
+    const resultadoImprime = ciclica.imprime();
+    setResultVerificar(resultadoImprime);
+    const resultadoRecorrer = ciclica.recorrer(0, longitud);
+    setResultRecorrerVer(resultadoRecorrer);
+    setIsResult(true)
+  }
+
+  return (
+
+    <>
+
       <StatusBar
-      backgroundColor='#01989F'/>
-      <AppBar 
-      color="#01989F" 
-      title="Simulador CRC"
-      leading={<Octicons name="file-binary" size={24} color="white" style={{paddingLeft: 5}} /> }
-      tintColor="white"
+        backgroundColor='#01989F' />
+      <AppBar
+        color="#01989F"
+        title="Simulador CRC"
+        leading={<Octicons name="file-binary" size={24} color="white" style={{ paddingLeft: 5 }} />}
+        tintColor="white"
       />
-      
+
       <ScrollView style={styles.scrollView}>
-      <View contentContainerStyle={styles.view}>
-          
-                <TextInput
-                    label="Dx"
-                    color="#1DD1CB"
-                    placeholder="x^7+ x^6 + x^5 + x +1"
-                    editable
-                    variant="standard"
-                    style={styles.input}
-                    onChangeText={handleDividendoChange}
-                    name="D"
-                    value={Dividendo}
-                />
-                <TextInput
-                    label="Gx"
-                    color="#1DD1CB"
-                    editable
-                    variant="standard"
-                    style={styles.input}
-                    onChangeText={handleDivisorChange}
-                    name="V"
-                    value={Divisor}
-                    placeholder="x^7+ x^6 + x^5 + x +1"
-                />
+        <View contentContainerStyle={styles.view}>
 
-                <Button style={styles.button}  title="Convertir" onPress={mostrarTextoClick}  />
+          <TextInput
+            label="Dx"
+            color="#1DD1CB"
+            placeholder="x^7+ x^6 + x^5 + x +1"
+            editable
+            variant="standard"
+            style={styles.input}
+            onChangeText={handleDividendoChange}
+            name="D"
+            value={Dividendo}
+            keyboardType="default"
+          />
 
-                <InputDx polynomial={inputCharge ? Dividendo : 'x'} ></InputDx>
-                <InputGx polynomial={inputCharge ? Divisor : 'x'} ></InputGx>
+          <TextInput
+            label="Gx"
+            color="#1DD1CB"
+            editable
+            variant="standard"
+            style={styles.input}
+            onChangeText={handleDivisorChange}
+            name="V"
+            value={Divisor}
+            placeholder="x^7+ x^6 + x^5 + x +1"
+          />
 
-                 
-                <Button style={styles.button} title="Dividir" onPress={calcularCRC} />
-                <Text>Resultados:</Text>
-                <Text>{resultado}</Text>
-                
+          <Button style={styles.button} title="Convertir" onPress={mostrarTextoClick} />
 
-                <Button style={styles.button}  title="Verificar" onPress={onResultSucces} />
-                <Text>Resultados:</Text>
-                <Text>{resultRecorrerVer}</Text>
-                    
-           
-          </View>
-          </ScrollView>
-          <AppBar 
-          variant="bottom"
-          title="by fernan | Marlon"
-          color="#F8F8F8" 
-          centerTitle={true}
-          titleStyle="" />
-          </>
-    )
+          {inputCharge ? (
+            <>
+              <InputDx polynomial={inputCharge ? Dividendo : 'x'} />
+              <InputGx polynomial={inputCharge ? Divisor : 'x'} />
+            </>
+          ) : (
+            (isNull.length > 2) && (
+              <Badge style={{ margin: 10 }} label={isNull} color="error" />
+            )
+          )}
+
+
+
+          {inputCharge ? (<>
+            <Button style={styles.button} title="Dividir" onPress={calcularCRC} />
+            {isVerificar ? (
+            <>
+            <Text>Resultados:</Text>
+            <Text>{resultado}</Text>
+            </>) : (<></>)}
+            
+          </>) : (<></>)}
+
+
+          {isVerificar ? (<>
+          <Button style={styles.button} title="Verificar" onPress={onResultSucces} />
+          {isResult ? (
+          <>
+          <Text>Resultados:</Text>
+          <Text>{resultRecorrerVer}</Text>
+          </>) : <></>}
+          </>) : (<></>)}
+
+        </View>
+      </ScrollView>
+      <Text
+        variant="bottom"
+        title="by fernan | Marlon"
+        color="#F8F8F8"
+        style={{
+           height: 20,
+           fontSize: 13,
+           color: 'gray',
+           textAlign: 'center',
+           justifyContent: "center",
+           alignContent: "center" }} > by fernan | Marlon </Text>
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -148,13 +195,13 @@ const styles = StyleSheet.create({
     alignContent: "center",
     padding: 16,
     backgroundColor: '#F8F8F8'
-   
+
   },
-  view:{
+  view: {
     alignItems: 'center',
     padding: 16,
     width: '100%',
-    
+
   },
   input: {
     height: 40,
@@ -162,10 +209,11 @@ const styles = StyleSheet.create({
     paddingLeft: 10,
     width: '100%',
   },
-  button:{
+  button: {
     width: '100%',
     backgroundColor: "#1DD1CB",
-    
+    marginTop: 10,
+
   }
 
 });
